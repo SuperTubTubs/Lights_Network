409bee70e4c46f884bb4b0bc4cee81419046e311
Hayden Wilson <hayden.wilson@eagles.oc.edu>
2018-06-15 09:03:10 -0500

    Initial commit

---
+++ java/com/hdub/lights/lightnetwork/LightsMain.java
@@ -0,0 +1,13 @@
+package com.hdub.lights.lightnetwork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LightsMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lights_main);
+    }
+}
